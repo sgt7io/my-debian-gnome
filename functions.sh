58d737407c535a8900bd6135c7c5103356366b6e
sgt7io <78266563+sgt7io@users.noreply.github.com>
2024-02-16 16:54:55 -0500

    Updates for Feb 16 part 1.

--- functions.sh
+++ functions.sh
@@ -6,7 +6,7 @@ update_debian() {
 }
 
 create_directories() {
-    local dirs=(~/.themes ~/.icons ~/software ~/Pictures/wallpapers)
+    local dirs=(~/.themes ~/.icons ~/software ~/scripts ~/Pictures/wallpapers)
     for dir in "${dirs[@]}"; do
         [ -d "$dir" ] || mkdir -p "$dir"
     done
@@ -19,17 +19,22 @@ copy_wallpapers() {
 install_packages() {
     while IFS= read -r package; do
         sudo apt -y install "$package"
-    done < "$script_dir/package_install.txt"
+    done < package_install.txt
+    echo 'Pacakges installed successfully.'
 }
 
 remove_packages() {
     while IFS= read -r package; do
         sudo apt -y purge "$package"
-    done < "$script_dir/package_remove.txt"
+    done < package_remove.txt
     sudo apt -y autoremove
+    echo 'Pacakges removed successfully.'
 }
 
 customize_gnome() {
+    local wallpaper_path="$HOME/Pictures/wallpapers/Wallpaper-House.com_391811.jpg"
+    gsettings set org.gnome.desktop.background picture-uri "file://$wallpaper_path"
+    gsettings set org.gnome.desktop.background picture-uri-dark "file://$wallpaper_path"
     gsettings set org.gnome.desktop.wm.preferences button-layout ':minimize,maximize,close'
 }
 
